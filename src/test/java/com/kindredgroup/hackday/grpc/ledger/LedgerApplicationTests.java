518e4fcab1dc1875a95bf10ff22683596e3972a1
mohkar <mohammadreza.sabourikarkhaneh@kindredgroup.com>
2019-05-16 14:24:31 +0200

    pSQL by default, H2 when local Remove unneeded protos

--- src/test/java/com/kindredgroup/hackday/grpc/ledger/LedgerApplicationTests.java
+++ src/test/java/com/kindredgroup/hackday/grpc/ledger/LedgerApplicationTests.java
@@ -6,8 +6,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("local")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LedgerApplicationTests {

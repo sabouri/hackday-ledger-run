f3b7ca6a6fcd2e8bd4185b44cef8f951fad9a37a
Jonas Fügedi <jonas.fugedi@kindredgroup.com>
2019-05-16 12:38:01 +0200

    Added initial code to handle gRPC requests

--- src/main/java/com/kindredgroup/hackday/grpc/ledger/LedgerServiceImpl.java
+++ src/main/java/com/kindredgroup/hackday/grpc/ledger/LedgerServiceImpl.java
@@ -4,27 +4,78 @@ import com.kindredgroup.hackday.grpc.LedgererviceGrpc;
 import com.unibet.wallet.infrastructure.protobuf.WalletClientProtos;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.util.Currency;
 
 @GrpcService
 public class LedgerServiceImpl extends LedgererviceGrpc.LedgererviceImplBase {
+    private final Bank bank;
+    private final static Currency bankCurrency = Currency.getInstance("SEK");
+
+    @Autowired
+    public LedgerServiceImpl(Bank bank) {
+        this.bank = bank;
+    }
+
     @Override
     public void balance(WalletClientProtos.BalanceRequest request, StreamObserver<WalletClientProtos.BalanceResponse> responseObserver) {
         super.balance(request, responseObserver);
-        request.getQualifiedUserName();
-        //responseObserver.onNext(new Ba);
+
+        String user = request.getQualifiedUserName();
+        BigDecimal amount = bank.balance(user);
+
+        WalletClientProtos.BalanceResponse.Builder balanceResponse = WalletClientProtos.BalanceResponse.newBuilder();
+        balanceResponse.setCash(amount.toPlainString());
+        balanceResponse.setCurrency(bankCurrency.getCurrencyCode());
+        balanceResponse.setQualifiedUserName(user);
+
+        responseObserver.onNext(balanceResponse.build());
+        responseObserver.onCompleted();
     }
 
     @Override
     public void deposit(WalletClientProtos.DepositRequest request, StreamObserver<WalletClientProtos.DepositResponse> responseObserver) {
         super.deposit(request, responseObserver);
-        request.getAmount();
 
-        //WalletClientProtos.DepositResponse.newBuilder().setAfterBalance()
-        //responseObserver.onNext();
+        String user = request.getQualifiedUserName();
+        BigDecimal amount = new BigDecimal(request.getAmount());
+
+        BigDecimal balanceAmount = bank.deposit(user, amount);
+
+        WalletClientProtos.DepositResponse.Builder builder = WalletClientProtos.DepositResponse.newBuilder();
+        builder.setAfterBalance(createCachBalance(balanceAmount));
+        builder.setApproved(true);
+        builder.addDistribution(createCashDistribution(amount));
+
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
     }
 
     @Override
     public void withdraw(WalletClientProtos.WithdrawRequest request, StreamObserver<WalletClientProtos.WithdrawResponse> responseObserver) {
         super.withdraw(request, responseObserver);
+
+        String user = request.getQualifiedUserName();
+        BigDecimal amount = new BigDecimal(request.getAmount());
+
+        BigDecimal balanceAmount = bank.withdraw(user, amount);
+
+        WalletClientProtos.WithdrawResponse.Builder builder = WalletClientProtos.WithdrawResponse.newBuilder();
+        builder.setAfterBalance(createCachBalance(balanceAmount));
+        builder.setApproved(true);
+        builder.addDistribution(createCashDistribution(amount));
+
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
+    }
+
+    private static WalletClientProtos.Balance createCachBalance(BigDecimal balanceAmount) {
+        return WalletClientProtos.Balance.newBuilder().setCash(balanceAmount.toPlainString()).setCurrency(bankCurrency.getCurrencyCode()).build();
+    }
+
+    private static WalletClientProtos.AmountDistribution createCashDistribution(BigDecimal amount) {
+        return WalletClientProtos.AmountDistribution.newBuilder().setAmount(amount.toPlainString()).setMoneyType("C").setCurrency(bankCurrency.getCurrencyCode()).build();
     }
 }
